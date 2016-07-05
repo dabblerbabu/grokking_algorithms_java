9fda8d9f1cf120baa85caf9d5b09432c6eebe125
Sridhar Srigiriraju <srigiriraju@gmail.com>
2016-07-05 01:05:01 -0700

    Added comments to BinarySearch class

--- chapter1/BinarySearch.java
+++ chapter1/BinarySearch.java
@@ -1,3 +1,6 @@
+/*
+This class is defined to implement Binary Search as an example.
+*/
 public class BinarySearch {
 	public int [] list = null;
 	public static void main(String [] args) {
@@ -6,7 +9,10 @@ public class BinarySearch {
 		BinarySearch bs = new BinarySearch();
 		System.out.println(bs.binarySearch(list, 10));
 	}
-	
+	/*
+		Pass a sorted list of array and the value the you
+		want to search within the array.
+	*/
 	public int binarySearch(int [] list, int value){
 		this.list = list;
 		int lowIndex = 0;
